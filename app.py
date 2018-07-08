83e9a7e2e65cddc22ea0f57a52ec9f58ba521912
kwugfighter <isaacli430@gmail.com>
2018-07-08 12:01:22 -0700

    added main css file

--- app.py
+++ app.py
@@ -13,47 +13,48 @@ class GlobalVars:
     def __init__(self):
         self.favicon = app.url_for('static', filename='favicon.png')
         self.logo_large = app.url_for('static', filename='logo-large.png')
+        self.main_css = app.url_for('static', filename='main.css')
 
 gvars = GlobalVars()
 
 @app.route('/')
 async def test(request):
     template = env.get_template('index.html')
-    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large)
+    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large, css=gvars.main_css)
     return html(html_content)
 
 @app.route('/about-us')
 async def test(request):
     template = env.get_template('aboutus.html')
-    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large)
+    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large, css=gvars.main_css)
     return html(html_content)
 
 @app.route('/features')
 async def test(request):
     global favicon
     template = env.get_template('features.html')
-    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large)
+    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large, css=gvars.main_css)
     return html(html_content)
 
 @app.route('/getting-started')
 async def test(request):
     global favicon
     template = env.get_template('gettingstarted.html')
-    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large)
+    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large, css=gvars.main_css)
     return html(html_content)
 
 @app.route('/signin')
 async def test(request):
     global favicon
     template = env.get_template('signin.html')
-    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large)
+    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large, css=gvars.main_css)
     return html(html_content)
 
 @app.route('/signup')
 async def test(request):
     global favicon
     template = env.get_template('signup.html')
-    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large)
+    html_content = template.render(favicon=gvars.favicon, logo_large=gvars.logo_large, css=gvars.main_css)
     return html(html_content)
 
 app.run(host="0.0.0.0", port=8000)
