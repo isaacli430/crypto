f2c75b51fffe65816fe549a44423c230f518a72b
kwugfighter <isaacli430@gmail.com>
2018-07-08 00:33:15 -0700

    switching to bootstrap

--- app.py
+++ app.py
@@ -8,15 +8,11 @@ env = Environment(loader=PackageLoader('app', 'templates'))
 
 app = Sanic(__name__)
 app.static('/static', './static')
-mat_css_url = app.url_for('static', filename='css/materialize.min.css')
-mat_js_url = app.url_for('static', filename='js/materialize.min.js')
 
 @app.route('/')
 async def test(request):
-    global mat_css_url
-    global mat_js_url
     template = env.get_template('index.html')
-    html_content = template.render(js=mat_js_url, css=mat_css_url)
+    html_content = template.render()
     return html(html_content)
 
 
